643a5d30703935d004d2136cb4ae595c9b1674e3
Jeroen <valcke_jeroen@hotmail.com>
2014-05-31 15:08:08 +0200

    context toegevoegd

---
+++ TalentFactory_CMS/DataAccess/Context/TFContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TalentFactory_CMS.DataAccess.Context
+{
+    public class TFContext : DbContext
+    {
+        public DbSet<Award> Award { get; set; }
+        public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+public DbSet<Contact> Contact { get; set; }
+        
+    }
+}
