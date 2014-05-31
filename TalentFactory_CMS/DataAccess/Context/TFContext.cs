c88e722f12ca4e754fc456e75adaf4bf6b231732
Jeroen <valcke_jeroen@hotmail.com>
2014-05-31 15:10:48 +0200

    context using toegevoegd (ook gwn een test)

--- TalentFactory_CMS/DataAccess/Context/TFContext.cs
+++ TalentFactory_CMS/DataAccess/Context/TFContext.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using TalentFactory_CMS.Models;
 
 namespace TalentFactory_CMS.DataAccess.Context
 {
