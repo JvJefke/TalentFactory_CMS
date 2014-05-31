0d382908e5df83fc84227a84e9c5d2440aafc48a
Jeroen <valcke_jeroen@hotmail.com>
2014-05-31 15:23:40 +0200

    whut???

---
+++ TalentFactory_CMS/DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿
+using TalentFactory_CMS.DataAccess.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TalentFactory_CMS.DataAccess.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class {
+
+        internal TFContext context;
+        internal DbSet<TEntity> dbSet;
+
+        public GenericRepository() {
+
+        }
+        public GenericRepository(TFContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> All() {
+            int aantal = dbSet.Count<TEntity>();
+            return dbSet;
+        }
+
+        public virtual TEntity GetByID(object id) {
+            return dbSet.Find(id);
+        }
+
+        public virtual TEntity Insert(TEntity entity) {
+            return dbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id) {
+            TEntity entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete) {
+            if (context.Entry(entityToDelete).State == EntityState.Detached) {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(TEntity entityToUpdate) {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+    }
+}
---
+++ TalentFactory_CMS/DataAccess/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TalentFactory_CMS.DataAccess.Repositories
+{
+
+    public interface IGenericRepository<TEntity>
+      where TEntity : class {
+        IEnumerable<TEntity> All();
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        TEntity GetByID(object id);
+        TEntity Insert(TEntity entity);
+        void Update(TEntity entityToUpdate);
+
+    }
+
+}
